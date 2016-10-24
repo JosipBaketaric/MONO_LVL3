783d87d248119803de441b893874cd589f930018
Your Name <you@example.com>
2016-10-24 13:02:59 +0200

    Added LVL3.Service and LVL3.Service.Common

---
+++ LVL3/LVL3.Service.Common/IVehicleService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LVL3.Model;
+using System.Collections;
+
+namespace LVL3.Service.Common
+{
+    public interface IVehicleService
+    {
+        bool Create(VehicleModel vehicleModel);
+        bool Create(VehicleMake vehicleMake);
+        VehicleMake ReadMake(int? id);
+        VehicleModel ReadModel(int? id);
+        bool Update(VehicleMake vehicleMake);
+        bool Update(VehicleModel vehicleModel);
+        bool DeleteMake(int? id);
+        bool DeleteModel(int? id);
+        IEnumerable ReadAllMakes();
+        IEnumerable ReadAllModels();
+        VehicleMake FindMakeById(int? id);
+        VehicleModel FindModelById(int? id);
+    }
+}
---
+++ LVL3/LVL3.Service/VehicleService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LVL3.Service.Common;
+using LVL3.DAL;
+using LVL3.Model;
+
+
+namespace LVL3.Service
+{
+    public class VehicleService : IVehicleService
+    {
+        private static VehicleService instance = null;
+        private VehicleContext db;
+        private VehicleService()
+        {
+            this.db = new VehicleContext();
+        }
+        public VehicleService GetInstance()
+        {
+            if (instance != null)
+                return instance;
+            instance = new VehicleService();
+            return instance;
+        }
+        public bool Create(VehicleMake vehicleMake)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Create(VehicleModel vehicleModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteMake(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteModel(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LVL3.Model.VehicleMake FindMakeById(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LVL3.Model.VehicleModel FindModelById(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable ReadAllMakes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable ReadAllModels()
+        {
+            throw new NotImplementedException();
+        }
+
+        public LVL3.Model.VehicleMake ReadMake(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LVL3.Model.VehicleModel ReadModel(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(LVL3.Model.VehicleModel vehicleModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(LVL3.Model.VehicleMake vehicleMake)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
